10403cddcc47992f9898bec9da16d3047cc070c1
Folarin Folarin <thefolafolarin@gmail.com>
2024-03-27 08:55:49 +0100

    updated code formating

--- index.js
+++ index.js
@@ -359,23 +359,23 @@
 // > Ex: It looks like a triangular "Play" button. <br><br>
 
 
-class Person {
-    constructor(name, age, energy) {
-        this.name = "Tom";
-        this.age = 20;
-        this.energy = 100;
-    }
+// class Person {
+//     constructor(name, age, energy) {
+//         this.name = "Tom";
+//         this.age = 20;
+//         this.energy = 100;
+//     }
 
 
-    sleep(){
-        this.energy += 10
-    }
+//     sleep(){
+//         this.energy += 10
+//     }
 
 
-    doSomethingFun(){
-        this.energy -= 10
-    }
-}
+//     doSomethingFun(){
+//         this.energy -= 10
+//     }
+// }
 
 
 
@@ -405,18 +405,18 @@ class Person {
 
 
 
-class Worker extends Person {
-    constructor(name,age,energy,xp,hourlyWage){
-        super (name,age,energy);
-        this.xp = 0;
-        this.hourlyWage = 10;
-    }
+// class Worker extends Person {
+//     constructor(name,age,energy,xp,hourlyWage){
+//         super (name,age,energy);
+//         this.xp = 0;
+//         this.hourlyWage = 10;
+//     }
 
 
-    goToWork(){
-        this.xp += 10
-    }
-}
+//     goToWork(){
+//         this.xp += 10
+//     }
+// }
 
 
 
@@ -441,11 +441,11 @@ class Worker extends Person {
 // <br><br>
 
 
-function intern(){
-    const internObject = new Worker ("bob", 21, 110, 0,10);
-    internObject.goToWork();
-    return internObject;
-}
+// function intern(){
+//     const internObject = new Worker ("bob", 21, 110, 0,10);
+//     internObject.goToWork();
+//     return internObject;
+// }
 
 // ## Task 3: Code a intern object
 
@@ -470,11 +470,11 @@ function intern(){
 
 
 
-function manager(){
-    const managerObject = new Worker ("Alice", 30, 120, 100,30);
-    managerObject.doSomethingFun();
-    return managerObject;
-}
+// function manager(){
+//     const managerObject = new Worker ("Alice", 30, 120, 100,30);
+//     managerObject.doSomethingFun();
+//     return managerObject;
+// }
 
 // ## Task 4: Code a manager object
 
@@ -1151,36 +1151,36 @@ function manager(){
 // Create another object representing a person with properties like name, age, and gender.
 
 
-class Car {
-    constructor (make,model,year){
-        this.make = `Toyota`,
-        this.model = `Convertible`,
-        this.year = 1998
-    }
+// class Car {
+//     constructor (make,model,year){
+//         this.make = `Toyota`,
+//         this.model = `Convertible`,
+//         this.year = 1998
+//     }
 
-carDetails(){
-    console.log(`this is a ${this.make} ${this.model} created in ${this.year}`) 
-}
+// carDetails(){
+//     console.log(`this is a ${this.make} ${this.model} created in ${this.year}`) 
+// }
 
-}
-
-
-let object = new Car;
-
-console.log(object.carDetails())
+// }
 
 
-class Personn {
-    constructor(name,age,gender){
-        this.name = `Abiola`,
-        this.age = 10,
-        this.gender = `Male`
-    }
-}
+// let object = new Car;
 
-let newPersonn = new Personn
+// console.log(object.carDetails())
 
-console.log(newPersonn);
+
+// class Personn {
+//     constructor(name,age,gender){
+//         this.name = `Abiola`,
+//         this.age = 10,
+//         this.gender = `Male`
+//     }
+// }
+
+// let newPersonn = new Personn
+
+// console.log(newPersonn);
 
 
 // Array Practice:
@@ -1190,41 +1190,41 @@ console.log(newPersonn);
 // Use the filter method to create a new array containing only the even numbers from an array of numbers.
 
 
-let arrNum = [1,2,3,4,5,6,7,8,9]
+// let arrNum = [1,2,3,4,5,6,7,8,9]
 
-arrNum.forEach(function (num)
-{ console.log(num)}
-)
+// arrNum.forEach(function (num)
+// { console.log(num)}
+// )
 
 // Sum of Array Elements:
 // Write a function that takes an array of numbers as input and calculates the sum of all elements using the forEach method.
 
-function getSum (arr){
-    let sum = 0
-    arr.forEach(function (num){
-        sum += num
-    })
-    return sum
-}
+// function getSum (arr){
+//     let sum = 0
+//     arr.forEach(function (num){
+//         sum += num
+//     })
+//     return sum
+// }
 
-let array = [1,2,3,5,6,7,8]
+// let array = [1,2,3,5,6,7,8]
 
-console.log(getSum(array));
+// console.log(getSum(array));
 
 
 // Logging Array Elements:
 // Write a function that takes an array of strings as input and logs each element to the console with a specific format (e.g., prepend each element with a number and a dash) using the forEach method.
 
-function arrString (arr){
- arr.forEach(function (num, index)
-    {console.log(`${index} - ${num}`)}
-    )
-}
+// function arrString (arr){
+//  arr.forEach(function (num, index)
+//     {console.log(`${index} - ${num}`)}
+//     )
+// }
 
 
-let strings = ["String", "Object", "Update"];
+// let strings = ["String", "Object", "Update"];
 
-console.log (arrString(strings));
+// console.log (arrString(strings));
 
 
 // Finding Maximum Element:
@@ -1459,3 +1459,304 @@ const employees = [
 
 console.log("Total salary of all employees:", calculateTotalSalary(employees));
 console.log("Name of the highest paid employee:", highestPaidEmployee(employees));
+
+
+// Nested Object Iteration:
+// Given a nested object representing a company's departments and employees, write a function called countEmployees that takes this object as an argument and returns the total number of employees in the company.
+
+
+function countEmployees(totalemployees){
+    let employeecount = 0;
+    for (let nested of totalemployees){
+        let result =  employeecount += nested
+        return result;
+    }
+
+}
+
+
+// Function to count the total number of employees in a nested object
+function countEmployees(company) {
+    let totalCount = 0;
+
+    // Iterate through each key in the object
+    for (let key in company) {
+        // Check if the value associated with the key is an object (department)
+        if (typeof company[key] === 'object') {
+            // Recursively count the employees in the nested object (department)
+            totalCount += countEmployees(company[key]);
+        } else {
+            // If the value is not an object, assume it's an employee and increment the count
+            totalCount++;
+        }
+    }
+
+    return totalCount;
+}
+
+// Example usage
+const company = {
+    'HR': {
+        'Manager': 'Alice',
+        'Assistant': 'Bob'
+    },
+    'IT': {
+        'Manager': 'Charlie',
+        'Developer': 'David',
+        'Tester': 'Eve'
+    }
+};
+
+console.log("Total number of employees:", countEmployees(company));
+
+
+// addEventListener()
+
+// const target = document.querySelector('body')
+
+// function handleClick(){
+//     console.log('clicked the body')
+// }
+
+// target.addEventListener('click',handleClick)
+
+// onclick = handleClick()
+
+
+
+
+// let answer = prompt('What is your name?');
+// if (typeof(answer) === 'string') {
+//     var h1 = document.createElement('h1')
+//     h1.innerText = answer;
+//     document.body.innerText = '';
+//     document.body.appendChild(h1);
+// }
+
+// addEventListener('change')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// # Lab Instructions: Little Lemon Receipt Maker
+ 
+// > ### **Tips: Before you Begin**
+// > #### **To view your code and instructions side-by-side**, select the following in your VSCode toolbar:
+// > - View -> Editor Layout -> Two Columns
+// > - To view this file in Preview mode, right click on this README.md file and `Open Preview`
+// > - Select your code file in the code tree, which will open it up in a new VSCode tab.
+// > - Drag your assessment code files over to the second column. 
+// > - Great work! You can now see instructions and code at the same time. 
+// > - Questions about using VSCode? Please see our support resources here:  
+// > [Visual Studio Code on Coursera](https://www.coursera.org/learn/programming-with-javascript/supplement/roMvE/visual-studio-code-on-coursera)
+// > #### **To run your JavaScript code**
+// > - Select your JavaScript file
+// > - Select the "Run Code" button in the upper right hand toolbar of VSCode.  
+// > Ex: It looks like a triangular "Play" button. <br><br>
+
+// <br>
+
+// ## Assignment Instructions
+
+// In this exercise, you will work with some data provided as an array of objects, listing information about dishes available in the Little Lemon restaurant.
+// <br><br>
+
+// **Step 1:** In the function `getPrices()`, give it the parameter of `taxBoolean`.
+
+// **Step 2:** Inside the `getPrices()` function, code a for loop that will loop over all the objects inside the `dishData` array.
+
+// **Step 3:** Inside the for loop, declare a `finalPrice` variable, without assigning it a value.
+
+// **Step 4:** Still inside the for loop, add an if condition, checking that the `taxBoolean` is set to `true`. Inside the if block, multiply the following: * the price of the currently looped-over object from the `dishData` array, and * the tax value. Assign the multiplied value to the finalPrice variable.
+
+// **Step 5:** Right after the if condition, add an else if, checking if the value of `taxBoolean` is false. Inside this condition's block, assign the currently looped-over dish price property in the `dishData` array to the `finalPrice` variable.
+// <br><br>
+
+// **Step 6:** Code the `else` case, and inside of it, add two lines of code:
+
+// A console log of the string:  
+
+// - "You need to pass a boolean to the getPrices call!"  
+
+// - return (to "jump out" of the further function execution)
+// <br><br>
+
+// <b>Step 7:</b> After all the conditional's statements, but still inside the for loop, code another console log with four arguments:
+
+// 1. The string `"Dish: "`
+
+// 2. The value of currently looped-over dish object's name property
+
+// 3. The string `"Price: $"`
+
+// 5. The value of the `finalPrice` variable
+// <br><br>
+
+// **Step 8:** You're finshed with the `getPrices()` function, and now you're ready to code another function. Give the `getDiscount()` function, two parameters: the `taxBoolean` and the `guests` parameter. 
+
+// **Step 9:** Inside the `getDiscount()` function,  on the very first line of its body, invoke the `getPrices()` function, passing it the `taxBoolean` as an argument.
+
+// **Step 10:** On another line, you need to implement your defensive coding skills, and check that the type of the `guests` parameter is 'number' and  that the value of the guests variable is greater than zero and less than 30.  If all these conditions return true, code the body of the conditional as described in the next step. If they don't all return true, code the body of the else conditional as instructed in step 12.
+
+// **Step 11:** Inside the if statment, declare a new variable, named `discount`, and set it to 0. On the next line, add another if...else if: in the first if, you'll check that the value of the `guests` variable is less than 5. If that's the case, reassign the value of the discount variable to 5; 
+// - Inside the else if condition, check that the value of the guests variable is greater than or equal to 5 - if that's the case, reassign the discount variable to 10.
+// - Console log the following after closing your else-if statement: `'Discount is: $' + discount);`
+
+
+// **Step 12:** In the else condition, console log the following string: `'The second argument must be a number between 0 and 30'`. Since you've finished declaring both the `getPrices()` and the `getDiscount()` functions, you can now invoke the `getDiscount()` function several times, with various combinations of arguments, to check the behavior. <br>
+
+// Here are two examples: 
+// - `getDiscount(true, 2) `
+// - `getDiscount(false, 10)`
+
+// What happens when you don't pass-in any arguments?  
+
+// What happens when you pass values that are not expected?
+
+
+// 1. Variable Declarations:
+
+// Declare variables to store your name, age, and favorite color. Assign appropriate values to each variable and print them to the console.
+
+
+let nameStr = `Abiola`;
+let age = 24;
+let favColor = `Black`;
+
+console.log(`my name is ${nameStr}, I am ${age}, my fav color is ${favColor}. `);
+
+
+const person = {
+    name: `Abiola`,
+    age: 24,
+    color: `Black`
+};
+
+
+console.log(`my name is ${person.name}, I am ${person.age}, my fav color is ${person.color}. `)
+
+
+// 2. Arithmetic Operations:
+
+// Write a program to calculate the area of a rectangle.
+//  Prompt the user to enter the width and height of the rectangle,
+//  calculate the area, and display the result.
+
+// let width = prompt(`What is the width of a rectangle`)
+// let height = prompt(`What is the height of a rectangle`)
+
+// function rectangle(sum){
+//     if (typeof(width) === Number && typeof(height) === Number  ) {
+//         let sum = width * height;
+//     }
+//     return sum
+// }
+
+
+
+
+
+
+
+// let width = prompt(`What is the width of a rectangle`);
+// let height = prompt(`What is the height of a rectangle`);
+
+// // Convert the input strings to numbers
+// width = parseFloat(width);
+// height = parseFloat(height);
+
+// function calculateRectangleArea(width, height) {
+//     // Check if both width and height are valid numbers
+//     if (!isNaN(width) && !isNaN(height)) {
+//         return width * height;
+//     } else {
+//         return "Invalid input. Please enter valid numbers for width and height.";
+//     }
+// }
+
+// console.log(calculateRectangleArea(6,9))
+
+// // Call the function to calculate the area
+// let area = calculateRectangleArea(width, height);
+
+// // Display the result
+// if (typeof area === 'number') {
+//     console.log(`The area of the rectangle is: ${area}`);
+// } else {
+//     console.log(area);
+// }
+
+
+
+
+
+
+
+// Conditional Statements:
+
+// Create variables to store two numbers. Write a program that compares the two numbers and prints whether the first number is greater than, less than, or equal to the second number.
+
+
+
+function comparism(x,y){
+  
+     if (x == 100 || y == 100){
+        console.log(true)
+    } else if (x + y == 100){
+        console.log(`equals 100`)
+    }
+    else{
+        console.log(`yada`)
+}
+    
+}
+
+let plus = 50;
+let minus = 50;
+
+console.log(comparism(plus,minus))
+
+
+function getFileExtension(extended){
+  let newmax = extended.slice(extended.lastIndexOf('.'))
+  return newmax
+}
+
+console.log(getFileExtension(`index.html`))
+
+
+
+
+
+
+
+
+
